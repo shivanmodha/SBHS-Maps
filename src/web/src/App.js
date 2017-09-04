ec73c8424d8111d13e3c2a88aada93298c5fd60c
Shivan Modha <shivanmodha@gmail.com>
2017-09-04 12:37:18 -0400

    Web: Clean Slate
    There are way too many things that need to be changed for this to work.

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MapComponent from './MapComponent';
 
-class App extends Component
-{
-    constructor(props)
-    {
-        super(props);
-    }
-    componentWillMount()
-    {
-        //let at = this.props.at.split(",");
-        //let last = at[2].split("z");
-        this.setState(
-        {
-            width: window.innerWidth + "px",
-            height: window.innerHeight + "px",
-            args:
-            {
-                x: 50,
-                y: 50,
-                z: 50,
-                f: 0
-            }
-        });
-    }
-    render()
-    {
-        return (
-            <div>
-                <MapComponent />
-            </div>
-        );
-    }
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
+
 export default App;
