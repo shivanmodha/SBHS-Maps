f7ef611569eac3ddbe0fe79420f5a062e5308093
Shivan Modha <shivanmodha@gmail.com>
2017-09-17 14:17:12 -0400

    Web: Element Detail Buttons

--- src/web/src/ElementDetails.js
+++ src/web/src/ElementDetails.js
@@ -24,7 +24,9 @@ class ElementDetails extends Component
                     <div>
                         <p className="lead" style={{ margin: 10 }}>{name}</p>
                     </div>
-                    <Button>Test</Button>
+                    <Button outline color="primary" style={{ borderRadius: 0, borderBottom: 0, borderLeft: 0, borderRight: 0 }}>Zoom</Button>
+                    <Button outline color="primary" style={{ borderRadius: 0, borderBottom: 0, borderLeft: 0, borderRight: 0 }}>From</Button>
+                    <Button outline color="primary" style={{ borderRadius: 0, borderBottom: 0, borderLeft: 0, borderRight: 0 }}>To</Button>
                 </div>
                 <Button outline style={{ position: "fixed", right: 8, top: 10, borderRadius: 0, border: 0, marginRight: 2 }} color="danger"><Icon style={{ padding: 0, margin: 0 }} name="close" /></Button>
             </ListGroupItem>
